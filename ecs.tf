c5388cbad45a2a7b9747173b47b284610e4335c9
Zee <zee@simplyzee.dev>
2021-08-29 18:08:55 +0100

    Adding ECS containerisation with fargate spot instances

---
+++ ecs.tf
@@ -0,0 +1,98 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+module "alb" {
+  source  = "umotif-public/alb/aws"
+  version = "~> 2.0"
+
+  name_prefix        = "alb-non-negative-integer"
+  load_balancer_type = "application"
+  internal           = false
+  vpc_id             = data.aws_vpc.default.id
+  subnets            = data.aws_subnet_ids.all.ids
+}
+
+resource "aws_lb_listener" "alb_listen_port_80" {
+  load_balancer_arn = module.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = module.ecs-fargate.target_group_arn[0]
+  }
+}
+
+resource "aws_security_group_rule" "alb_ingress_80" {
+  security_group_id = module.alb.security_group_id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "task_ingress_80" {
+  security_group_id        = module.ecs-fargate.service_sg_id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = module.alb.security_group_id
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "non-negative-ecs-clustr"
+
+  capacity_providers = ["FARGATE_SPOT"]
+  
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+  }
+
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+}
+
+module "ecs-fargate" {
+  source = "umotif-public/ecs-fargate/aws"
+  version = "~> 6.1.0"
+
+  name_prefix = "non-negative"
+  vpc_id             = data.aws_vpc.default.id
+  private_subnet_ids = data.aws_subnet_ids.all.ids
+  cluster_id = aws_ecs_cluster.cluster.id
+  task_container_image   = "zeemarsh/non-negative-integer:0.0.1"
+  task_definition_cpu    = 256
+  task_definition_memory = 512
+
+  task_container_port             = 80
+  task_container_assign_public_ip = true
+
+  target_groups = [
+    {
+      target_group_name = "non-negative-integer"
+      container_port    = 80
+    }
+  ]
+
+  health_check = {
+    port = "traffic-port"
+    path = "/"
+  }
+
+  capacity_provider_strategy = [
+    {
+      capacity_provider = "FARGATE_SPOT",
+      weight            = 100
+    }
+  ]
+}
